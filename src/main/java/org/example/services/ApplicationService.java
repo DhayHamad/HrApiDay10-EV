1f6cbf9087acc9ad3cd5f7bf7fd7f625c8818586
dhay alduaylij <dhayaldaeilij@gmail.com>
2024-06-10 07:45:52 +0300

    Evaluation Day (10) part 1

---
+++ src/main/java/org/example/services/ApplicationService.java
@@ -0,0 +1,13 @@
+package org.example.services;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+
+@ApplicationScoped
+public class ApplicationService {
+
+    private int count;
+
+    public int getCount() {
+        return ++count;
+    }
+}
